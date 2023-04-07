9a692d524d629c20d4e7a3c173f9eef88687e562
Bastiangm23 <bastian.garcia.m@gmail.com>
2023-04-06 21:13:34 -0400

    comit

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@ const divResultado = document.querySelector(".field-resultado");
 divResultado.style.visibility = "hidden";
 
 function CalcularTMB() {
-  
   if (
     sexo.value != "" &&
     altura.value != "" &&
@@ -19,48 +18,45 @@ function CalcularTMB() {
   ) {
     console.log("Entrando a la funcion 'CalcularTMB'");
     if (sexo.value == "masculino") {
-        // Hombres TMB = (10 x peso de Kg) + (6,25 x altura en cm) – (5 x edad en años) + 5
-        resultado.value =
-          10 * parseInt(peso.value) +
-          6.25 * parseInt(altura.value) -
-          5 * parseInt(edad.value) +
-          5; // formula para conseguir el TMB de una hombre
-        console.log("Es hombre");
-      } else {
-        // Mujeres TMB = (10 x peso en kg) + (6,25 x altura en cm) – (5 x edad en años) – 161
-        resultado.value =
-          10 * parseInt(peso.value) +
-          6.25 * parseInt(altura.value) -
-          5 * parseInt(edad.value) -
-          161; // formula para conseguir el TMB de una mujer
-        console.log("Es mujer");
-      }
+      // Hombres TMB = (10 x peso de Kg) + (6,25 x altura en cm) – (5 x edad en años) + 5
+      resultado.value =
+        10 * parseInt(peso.value) +
+        6.25 * parseInt(altura.value) -
+        5 * parseInt(edad.value) +
+        5; // formula para conseguir el TMB de una hombre
+      console.log("Es hombre");
+    } else {
+      // Mujeres TMB = (10 x peso en kg) + (6,25 x altura en cm) – (5 x edad en años) – 161
+      resultado.value =
+        10 * parseInt(peso.value) +
+        6.25 * parseInt(altura.value) -
+        5 * parseInt(edad.value) -
+        161; // formula para conseguir el TMB de una mujer
+      console.log("Es mujer");
+    }
+
+    //Acontinuacion cambiara el resultado dependiendo de que opcion haya elegido el usuario en "Nivel de actividad".
+
+    if (nivelActividad.value == "poco") {
+      resultado.value = resultado.value * 1.2;
+    }
+    if (nivelActividad.value == "ligero") {
+      resultado.value = resultado.value * 1.375;
+    }
+    if (nivelActividad.value == "moderado") {
+      resultado.value = resultado.value * 1.55;
+    }
+    if (nivelActividad.value == "fuerte") {
+      resultado.value = resultado.value * 1.725;
+    }
+    if (nivelActividad.value == "muyfuerte") {
+      resultado.value = resultado.value * 1.9;
+    }
     
-      //Acontinuacion cambiara el resultado dependiendo de que opcion haya elegido el usuario en "Nivel de actividad".
-    
-      if (nivelActividad.value == "poco") {
-        resultado.value = resultado.value * 1.2;
-      }
-      if (nivelActividad.value == "ligero") {
-        resultado.value = resultado.value * 1.375;
-      }
-      if (nivelActividad.value == "moderado") {
-        resultado.value = resultado.value * 1.55;
-      }
-      if (nivelActividad.value == "fuerte") {
-        resultado.value = resultado.value * 1.725;
-      }
-      if (nivelActividad.value == "muyfuerte") {
-        resultado.value = resultado.value * 1.9;
-      }
-    
-      divResultado.style.visibility = "visible";
-    
-  }else{
+    divResultado.style.visibility = "visible";
+  } else {
     console.log("ERROR: Debe rellenar todos los campos");
   }
 
-  
-
-  
+    
 }
